0a8027a21279706883518d5e08d47b2d8b7f8473
Aleksei Pugachev <lenyapugachev@me.com>
2023-01-07 04:37:30 +0300

    Fix 3

--- src/play.c
+++ src/play.c
@@ -81,8 +81,6 @@ void playSequence(char* sequence)
         Paint_NewImage(BlackImage, LCD_WIDTH, LCD_HEIGHT, 0, BLACK, 16);
         Paint_Clear(BLACK);
 
-        char one;
-        one = "1";
         size_t i;
         for (i = 576 * f; i < 576 * (f + 1); i++) {
             int row;
@@ -90,7 +88,7 @@ void playSequence(char* sequence)
             int col;
             col = (i - 576 * f) % 24;
             printf("%c", sequence[i]);
-            Paint_DrawRectangle(2 + col * 10, 2 + row * 10, 10 + col * 10, 10 + row * 10, sequence[i] == one ? WHITE : BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
+            Paint_DrawRectangle(2 + col * 10, 2 + row * 10, 10 + col * 10, 10 + row * 10, sequence[i] == '1' ? WHITE : BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
         }
 
         LCD_1in3_Display(BlackImage);
